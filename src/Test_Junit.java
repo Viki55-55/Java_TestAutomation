96f3b6d32ca1b37c1dce4194f588dafc41d50b85
Viki55-55 <solliss@live.se>
2023-02-23 12:42:46 +0100

    test

--- src/Test_Junit.java
+++ src/Test_Junit.java
@@ -4,18 +4,30 @@ import org.junit.Test;
 
 public class TestingwithJUnit {
     @Test
-    public void TestGetRows1() {
+    public void Test1() {
         Morse myInput_Words = new Morse();
         int Actual_Result = myInput_Words.getInput();
         int Expected_Result = 0;
         Assert.assertEquals(Actual_Result, Expected_Result);
     }
     @Test
-    public void TestGetRows2() {
+    public void Test2() {
         Morse myInput_Words = new Morse();
         myInput_Words.setInput("some text");
         int Actual_Result = myInput_Words.getInput();
         int Expected_Result = 1;
         Assert.assertEquals(Actual_Result, Expected_Result);
     }
-}
+    @Test
+    public void return_check(){
+        public void return_check_char(){
+            assertThat(Morse.get("-")).isEqualTo("a");
+        }
+
+        @Test
+        public void return_word_check(){
+            public void retur_word_check_Morse(){
+                assertThat(Morse.get("---")).isEqualTo("aaa");
+            }
+
+        }
